b435f3615c54bfec752bc4ad649bb1894ce7da8d
maxq10 <chris@robwanenchak.com>
2016-07-13 22:25:55 -0400

    styling header

--- wp-content/themes/robw_photos/header.php
+++ wp-content/themes/robw_photos/header.php
@@ -28,14 +28,22 @@
 
 	<header id="masthead" class="site-header" role="banner">
             
-            <h1 class="title">rob<span>wanenchak</span>photography</h1>
-            
 		<div class="site-branding">
+                    <article class="robw-title">
 			<?php
-			if ( is_front_page() && is_home() ) : ?>
+                        //Commenting out default site title
+			/*if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="//<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
 				<p class="site-title"><a href="//<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;*/
+                        
+                        //Custom site title
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="title"><a href="//<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">rob<span>wanenchak</span>photography</a></h1>
+			<?php else : ?>
+				<p class="title"><a href="//<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">rob<span>wanenchak</span>photography</a></p>
 			<?php
 			endif;
 
@@ -44,12 +52,15 @@
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
 			endif; ?>
+                    </article>
 		</div><!-- .site-branding -->
 
+                <article class="robw-nav">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'robw_photos' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
+                </article>
 	</header><!-- #masthead -->
 </div><!-- .container -->
 </section>
